90f3168cc14551d7e620565418a76724b621f90d
WH <whitneyhuong+gitkraken@gmail.com>
2021-11-20 13:51:44 -0500

    fix padding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Menu, Home, Gallery, Contact, Footer } from './components';
-import useWindow from './hooks/useWindow';
 import Preloader from './components/preloader/Preloader';
 
 function App() {
-
-  useWindow()
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
 
   return (
     <div className="App">
