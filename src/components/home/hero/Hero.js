c7eac9370c67f984e3ac2f2395ba9cbd06a53859
Whitney <designedbywh@gmail.com>
2023-01-23 00:00:33 -0500

    added a test get request to hero section

--- src/components/home/hero/Hero.js
+++ src/components/home/hero/Hero.js
@@ -1,7 +1,28 @@
 import './hero.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Hero() {
+  const [messages, setMessages] = useState([]);
 
+  const update = () => {
+    console.log("clicked")
+    axios.get("/new")
+    .then(res => {
+      console.log("here")
+      setMessages(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const allMessages = messages.map(message => {
+    return (
+      <div key={message.name}>Hi my name is {message.name}, and my age is {message.age}</div>
+    )
+  })
+  
   return (
     <section className="hero">
       <div className="hero_bg">
@@ -9,6 +30,7 @@ function Hero() {
           <h2 id="slogan">
             Awaken your inner <br />goddess
           </h2>
+          {messages.length > 0 ? <div>{allMessages}</div> : <div onClick={update}>Click me</div>}
         </div>
       </div>
     </section>
